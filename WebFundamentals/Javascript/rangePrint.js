c318894088b30ab3883a5d2af93429d736b2e5c2
Davidjohn Blodgett <davidjohn.blodgett@gmail.com>
2017-07-23 15:01:07 -0700

    rangePrint exercise complete

---
+++ WebFundamentals/Javascript/rangePrint.js
@@ -0,0 +1,21 @@
+function rangePrint(start,end,skip){
+    if(skip === undefined){
+        skip = 1;
+    }
+    if(end === undefined){
+        end = start;
+        start = 0;
+    }
+    for(var i = start;i < end;i = i + skip){
+        console.log(i);
+    }
+}
+
+console.log("####");
+rangePrint(2,10,2);
+console.log("####");
+rangePrint(4,8);
+console.log("####");
+rangePrint(4);
+console.log("####");
+rangePrint(-2,5,2)
