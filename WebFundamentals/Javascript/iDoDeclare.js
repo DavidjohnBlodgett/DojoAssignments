353253f554503ab5284cba103124b7f1b186c5fd
Davidjohn Blodgett <davidjohn.blodgett@gmail.com>
2017-07-23 12:01:58 -0700

    updated .gitignore and added a JavaScript problem

---
+++ WebFundamentals/Javascript/iDoDeclare.js
@@ -0,0 +1,28 @@
+var num1 = 46;
+var num2 = 12;
+var num3 = -1;
+var num4 = 5.65;
+
+var string1 = "Hello";
+var string2 = "World";
+var string3 = "!!!";
+
+var bool1 = true;
+var bool2 = false;
+
+var undef1;
+
+
+console.log("First Number is: " + num1);
+console.log("Second Number is: " + num2);
+console.log("Third Number is: " + num3);
+console.log("Fourth Number is: " + num4);
+
+console.log("First String is: " + string1);
+console.log("Second String is: " + string2);
+console.log("Third String is: " + string3);
+
+console.log("First Boolean is: " + bool1);
+console.log("First Boolean is: " + bool2);
+
+console.log("The undef is: " + undef1);
