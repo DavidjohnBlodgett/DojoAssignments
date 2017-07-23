337ab7137a759160bca456b061069b14479e802c
Davidjohn Blodgett <davidjohn.blodgett@gmail.com>
2017-07-23 13:48:27 -0700

    finsihed whileYouWait exercise

---
+++ WebFundamentals/Javascript/whileYouWait.js
@@ -0,0 +1,13 @@
+var daysToBirthday = 60;
+
+while(daysToBirthday > 0){
+    if(daysToBirthday > 10){
+        console.log("",daysToBirthday, "days until my birthday. Such a long time... :(");
+    }else if (daysToBirthday <= 10) {
+        console.log("", daysToBirthday, "DAYS UNTIL MY BIRTHDAY!!!");
+    }
+    daysToBirthday--;
+}
+console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*");
+console.log("♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪");
+console.log("*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«");
