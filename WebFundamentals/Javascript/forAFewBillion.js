98471846ef8e5b48c539871db1040bd20040ca6f
Davidjohn Blodgett <davidjohn.blodgett@gmail.com>
2017-07-23 14:21:48 -0700

    completed forAFewBillion exercise

---
+++ WebFundamentals/Javascript/forAFewBillion.js
@@ -0,0 +1,47 @@
+// sum of 30days of work...
+var sum = 0.01;
+
+for(var i = 1; i <= 30; i++){
+    sum = sum * 2;
+}
+console.log("sum of 30days of work...");
+console.log(sum);
+
+// days of work to get 10k...
+var sum = 0.01;
+var days = 1;
+
+while(sum < 10000){
+    sum = sum * 2;
+    days++;
+}
+
+console.log("days of work to get 10k...");
+//console.log(sum);
+console.log(days);
+
+// days of work to get 1 Billion...
+var sum = 0.01;
+var days = 1;
+
+while(sum < 1000000000){
+    sum = sum * 2;
+    days++;
+}
+
+console.log("days of work to get 1 Billion...");
+//console.log(sum);
+console.log(days);
+
+// days of work to get Infinity...
+var sum = 0.01;
+var days = 1;
+
+while(sum < Infinity){
+    sum = sum * 2;
+    days++;
+}
+
+console.log("days of work to get Infinity...");
+//console.log(sum);
+console.log(days);
