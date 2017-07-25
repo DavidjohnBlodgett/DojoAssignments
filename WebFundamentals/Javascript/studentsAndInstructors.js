043d43dc63071ead9e6ce410dea8c418973f46cf
Davidjohn Blodgett <davidjohn.blodgett@gmail.com>
2017-07-24 18:02:09 -0700

    studentsAndInstructors exercise complete

---
+++ WebFundamentals/Javascript/studentsAndInstructors.js
@@ -0,0 +1,35 @@
+// part 1...
+var students = [
+     {first_name:  'Michael', last_name : 'Jordan'},
+     {first_name : 'John', last_name : 'Rosales'},
+     {first_name : 'Mark', last_name : 'Guillen'},
+     {first_name : 'KB', last_name : 'Tonel'}
+]
+
+for(var i = 0;i<students.length;i++){
+    console.log(students[i].first_name + ' ' + students[i].last_name);
+}
+
+console.log("####");
+
+// part 2...
+var users = {
+ 'Students': [
+     {first_name:  'Michael', last_name : 'Jordan'},
+     {first_name : 'John', last_name : 'Rosales'},
+     {first_name : 'Mark', last_name : 'Guillen'},
+     {first_name : 'KB', last_name : 'Tonel'}
+  ],
+ 'Instructors': [
+     {first_name : 'Michael', last_name : 'Choi'},
+     {first_name : 'Martin', last_name : 'Puryear'}
+  ]
+ }
+
+ for ( var user in users ) {
+     console.log(user.toUpperCase());
+     for (var i = 0; i < users[user].length; i++) {
+         var name = users[user][i].first_name + ' ' + users[user][i].last_name;
+         console.log( i + ' ' + name + ' ' + (name.length-1));
+     }
+ }
