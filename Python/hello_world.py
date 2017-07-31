6312b9ea28f819f6b99c12b40d66466d4804991b
Davidjohn Blodgett <davidjohn.blodgett@gmail.com>
2017-07-31 08:37:45 -0700

    first Python commit, hello world fun

---
+++ Python/hello_world.py
@@ -0,0 +1,18 @@
+print "Hello World!"
+
+x = "Hello Python"
+print x
+y = 42
+print y
+
+
+name = "DJ"
+
+def say_hello(name):
+    if name:
+        print "hello, " + name + " welcome..."
+    else:
+        print "never made the if"
+
+say_hello(name)
+print "EOF"
