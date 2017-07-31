03c1fc4d38171b6e5ddb7462f9c3108d5229564e
Davidjohn Blodgett <davidjohn.blodgett@gmail.com>
2017-07-31 14:07:53 -0700

    filterByType exercise complete

---
+++ Python/filterByType.py
@@ -0,0 +1,52 @@
+# TEST VALUES
+sI = 45
+mI = 100
+bI = 455
+eI = 0
+spI = -23
+sS = "Rubber baby buggy bumpers"
+mS = "Experience is simply the name we give our mistakes"
+bS = "Tell me and I forget. Teach me and I remember. Involve me and I learn."
+eS = ""
+aL = [1,7,4,21]
+mL = [3,5,7,34,3,2,113,65,8,89]
+lL = [4,34,22,68,9,13,3,5,7,9,2,12,45,923]
+eL = []
+spL = ['name','address','phone number','social security number']
+
+
+def filterType(var):
+    if(type(var) is int):
+        print "I found an int!"
+        if(var >= 100):
+            print "That's a big number!"
+        else:
+            print "That's a small number"
+    if(type(var) is str):
+        print "I found an string!"
+        if(len(var) >= 50):
+            print "Long sentence."
+        else:
+            print "Short sentence."
+    if(type(var) is list):
+        print "I found an list!"
+        if(len(var) >= 10):
+            print "Big list!"
+        else:
+            print "Short list."
+
+# test...
+filterType(sI)
+filterType(mI)
+filterType(bI)
+filterType(eI)
+filterType(spI)
+filterType(sS)
+filterType(mS)
+filterType(bS)
+filterType(eS)
+filterType(aL)
+filterType(mL)
+filterType(lL)
+filterType(eL)
+filterType(spL)
