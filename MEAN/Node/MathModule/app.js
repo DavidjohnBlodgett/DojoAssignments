3fcb67dafe3db89ba0c159f5dc082f73a8a50e3e
Davidjohn Blodgett <davidjohn.blodgett@gmail.com>
2017-09-07 17:46:06 -0700

    math module complete and some ref files

---
+++ MEAN/Node/MathModule/app.js
@@ -0,0 +1,6 @@
+var math = require('./mathlib')();
+
+console.log(math.add(1,9));
+console.log(math.multiply(1,9));
+console.log(math.square(1,9));
+console.log(math.random(1,9));
---
+++ MEAN/Node/node_module_basics/app.js
@@ -0,0 +1,5 @@
+var my_module = require('./my_module');
+// OR if the export is a function call, notice the ()...
+// var my_module = require('./my_module')();
+my_module.greet();
+my_module.add(5,6);
