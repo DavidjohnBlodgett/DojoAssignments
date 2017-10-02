bd961afd91c6a0834e9c7ed41b59c0541af7ae23
Davidjohn Blodgett <davidjohn.blodgett@gmail.com>
2017-10-02 16:11:50 -0700

    boxing unboxing complete

---
+++ netCore/LanguageEssentials/BoxingUnboxing/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            List<object> myList = new List<object>();
+
+            myList.Add(7);
+            myList.Add(28);
+            myList.Add(-1);
+            myList.Add(true);
+            myList.Add("chair");
+
+            for (int i =0; i < myList.Count; i++){
+                Console.WriteLine(myList[i]);
+            }
+
+            int sum = 0;
+            for (int i =0; i < myList.Count; i++){
+                if (myList[i] is int) {
+                    sum = sum + (int)myList[i];
+                }
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
